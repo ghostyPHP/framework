ddbdb397e5948ab39492dbbe7f72d803bc88201d
AnvilM <211anvil112@gmail.com>
2024-06-07 21:39:13 +0300

    Add filesystem facade

---
+++ src/Support/Facades/File.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Ghosty\Support\Facades;
+
+use Ghosty\Filesystem\Filesystem;
+
+
+/**
+ * @method static string get(string $path) Get file content
+ * @method static bool put(string $path, string $data) Put data to file
+ * @method static string add(string $path, string $data) Add data to file
+ */
+class File
+{
+    public static function getFacadeName()
+    {
+        return Filesystem::class;
+    }
+}
